b4356d0610ebbf824aa075b0f827b5feb1c2e6eb
HalefBudanur5449 <65599762+HalefBudanur5449@users.noreply.github.com>
2020-05-19 17:47:24 +0300

    Add files via upload

---
+++ Odev/build/generated-sources/ap-source-output/odev/Otopark_.java
@@ -0,0 +1,18 @@
+package odev;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-19T17:21:14")
+@StaticMetamodel(Otopark.class)
+public class Otopark_ { 
+
+    public static volatile SingularAttribute<Otopark, Integer> numara;
+    public static volatile SingularAttribute<Otopark, String> adi;
+    public static volatile SingularAttribute<Otopark, String> plaka;
+    public static volatile SingularAttribute<Otopark, String> soyadi;
+    public static volatile SingularAttribute<Otopark, Integer> id;
+    public static volatile SingularAttribute<Otopark, Integer> girissaati;
+
+}
